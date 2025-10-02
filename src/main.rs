ee90517641bb6a9b3573b6a9178ea0c4df53607d
WangWindow <1598593280@qq.com>
2025-10-02 19:48:51 +0800

    [update]: 更新 `main.rs` 文件头注释中的信息

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 /*
+==============================================================================
  * @FilePath: main.rs
- * @Author: WangWindow 1598593280@qq.com
- * @Date: 2025-09-27 22:30:32
+ * @Author: <WangWindow 1598593280@qq.com>
+ * @Date: 2025-09-29 19:54:50
  * @LastEditors: WangWindow
- * @LastEditTime: 2025-09-27 23:37:06
- * 2025 by WangWindow, All Rights Reserved.
+ * @LastEditTime: 2025-10-02 19:47:39
+ * @2025 by WangWindow, All Rights Reserved.
  * @Descripttion:
- */
+==============================================================================
+*/
 
 fn main() {
     leetcode_rs::run();
